778ae9f0915ede70e74b1b4f8c34075191cb2a2c
juanprzo <juanp.przo26@gmail.com>
2020-07-15 17:39:34 -0400

    other

--- app.js
+++ app.js
@@ -196,11 +196,7 @@ app.post("/login", function(req, res){
     });
 });
 
-let port = process.env.PORT
-if (port == null || port == "") {
-  port = 3000
-}
-
-app.listen(3000, function() {
-    console.log("Server started on port 3000.");
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server listening on port 3000');
+    
 });
